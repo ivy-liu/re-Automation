3faed11dde211e587cdb056193dae4928dbee3d8
ivy liu <ivy.liu.liuwanying@outlook.com>
2019-07-25 17:57:20 +0800

    上午模块文件异常其他

---
+++ 0318_01_Module_file_exception_other.py
@@ -0,0 +1,24 @@
+#2018-3-18上午模块文件异常其他
+
+def bubble_sort(nums):
+    for i in range(len(nums) - 1):  # 这个循环负责设置冒泡排序进行的次数
+        for j in range(len(nums) - i - 1):  # j为列表下标
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
+ 
+print(bubble_sort([45, 32, 8, 33, 12, 22, 19, 97]))
+# 输出：[8, 12, 19, 22, 32, 33, 45, 97]
+
+
+print('\n---from 文件名---')
+import sum
+print(sum.add_two_num(1,2))
+
+print('\n---from 文件名 import 函数/类----')
+from sum import add_two_num
+print(add_two_num(2,2))
+
+print('\n---from 文件名 import 文件名----')
+from re_Automation import sum
+print(sum.add_two_num(2,3))
---
+++ sum.py
@@ -0,0 +1,4 @@
+def add_two_num(a,b):
+    sum=a+b
+    print('\n-调用sum.py sum函数-\n')
+    return sum
