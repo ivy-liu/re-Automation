674d3438e268de95a819bb657d4b823415e8071d
ivy liu <ivy.liu.liuwanying@outlook.com>
2019-07-30 22:26:02 +0800

    0110

---
+++ 0321_mysql_and_excel.py
@@ -0,0 +1,104 @@
+import mysql.connector
+
+# 连接数据库
+try:
+    con = mysql.connector.connect(
+        host='127.0.0.1',
+        user='root',
+        password='vertrigo',
+        port=3306,
+        database='xiaomingtest',
+        charset='utf8',
+        buffered=True
+    )
+    print('数据库连接成功')
+    cursor = con.cursor()  # 获取游标
+except mysql.connector.Error as e:
+    print('数据库连接失败', str(e))
+
+
+# 创建student表  auto_increment primary key主键自增，创建数据时不需要再写入
+Student = '''
+    create table Student(
+        StdID int auto_increment primary key,
+        StdName varchar(100) not null,
+        Gender enum('M','F') not null,
+        Age int
+    )
+'''
+
+try:
+    cursor.execute(Student)
+    print('创建表成功')
+except Exception as e:
+
+    print('创建表失败', str(e))
+# finally:
+#     cursor.close()
+#     print('关闭数据库连接')
+
+# 插入数据
+
+try:
+    # 字符串插入
+    print('----字符串插入---')
+
+    stu_info = '''
+    insert into Student(StdName,Gender,Age)
+    values('霸王龙','M',27)
+    '''
+    cursor.execute(stu_info)
+
+    # 元组插入
+    print('----元组插入---')
+    # 此处%s为占位符，不是格式化字符串，所以age用%s
+    stu_info_tup = '''
+    insert into student(StdName,Gender,Age)values(%s,%s,%s)
+    '''
+    data = ('小强', 'M', 21)
+    cursor.execute(stu_info_tup, data)
+
+    # 提交操作，不提交不生效，2选1
+    con.commit()
+    # cursor.execute('commit')
+
+    print('插入数据成功')
+except Exception as e:
+    print('插入失败', str(e))
+
+# 查询数据
+stu_select = '''
+    select *from Student where StdID>5
+    '''
+try:
+    cursor.execute(stu_select)
+    # 获取所有记录列表 
+    # fetchall 抓取全部 
+    # fetchmany(2)返回2条数据 
+    # fetchone() result[0],result[1],result[2]得到StdID,StdName,Gender...
+    results = cursor.fetchall()
+    for row in results:
+        StdID = row[0]
+        StdName = row[1]
+        Gender = row[2]
+        Age = row[3]
+    
+        # 打印结果
+        print ("StdID=%s,StdName=%s,Gender=%s,Age=%s"% 
+        (StdID, StdName, Gender, Age))
+except Exception as e:
+    print('查询失败', str(e))
+
+
+
+cursor.close()  # 关闭游标
+con.close()  # 关闭连接
+
+
+
+'''
+1.连接数据库
+2.创建游标
+3.执行cursor.execute(stu_info)，‘stu_info’SQL语句
+4.cursor.close()  # 关闭游标  con.close()  # 关闭连接
+'''
--- README.md
+++ README.md
@@ -66,6 +66,31 @@ http://localhost/phpmyadmin/index.php
 
 
 
+* 数据库插入数据
+---
+```python
+# 元组插入
+print('----元组插入---')
+# 此处%s为占位符，不是格式化字符串，所以age用%s
+stu_info_tup = '''
+insert into student(StdName,Gender,Age)values(%s,%s,%s)
+'''
+data = ('小强', 'M', 21)
+cursor.execute(stu_info_tup, data)
+
+# 提交操作，不提交不生效，2选1
+con.commit()
+# cursor.execute('commit')
+
+cursor.close()  # 关闭游标
+con.close()  # 关闭连接
+
+# 1.连接数据库
+# 2.创建游标
+# 3.执行cursor.execute(stu_info)，‘stu_info’SQL语句
+# 4.cursor.close()  # 关闭游标  con.close()  # 关闭连接
+
+```
 
 
 
