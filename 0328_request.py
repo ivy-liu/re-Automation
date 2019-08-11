2fd7ee05e943bb807ea28a6485940e307322e173
ivy liu <ivy.liu.liuwanying@outlook.com>
2019-08-11 22:05:45 +0800

    2205

---
+++ 0328_request.py
@@ -0,0 +1,52 @@
+import requests
+import json
+'''
+url="http://localhost:12306"
+path="/book_sim"
+
+full_url=url+path
+print("GET请求完整url=",full_url)
+
+params={
+    "book_name":"xiaoming",
+    "check_status":"on"         
+}
+print("GET请求参数=",params)
+
+#以json形式提交要写，不写默认以form形式提交
+headers={}
+
+r=requests.get(full_url,params=params,headers=headers)
+print("GET响应状态码=",r.status_code)
+print("GET响应头=",r.headers)#'Content-Type': 'application/json; charset=gbk'
+print("GET响应结果（json类型）=",r.text)
+print("GET接口的响应时间=",r.elapsed.total_seconds(),'秒')
+
+json_r=r.json()
+print("GET响应结果（转为python类型，供后续使用）=",json_r)
+'''
+
+
+url = "http://localhost:12306"
+path = "/book_info"
+
+full_url = url+path
+print("GET请求完整url=", full_url)
+
+params = {
+    "bookname": "接口来自moco",
+    "checkstatus": "on"
+}
+print("GET请求参数=", params)
+
+# 以json形式提交要写，不写默认以form形式提交
+headers = {}
+
+r = requests.get(full_url, params=params, headers=headers)
+print("GET响应状态码=", r.status_code)
+print("GET响应头=", r.headers)  # 'Content-Type': 'application/json; charset=gbk'
+print("GET响应结果（json类型）=", r.text)
+print("GET接口的响应时间=", r.elapsed.total_seconds(), '秒')
+
+json_r = r.json()
+print("GET响应结果（转为python类型，供后续使用）=", json_r)
