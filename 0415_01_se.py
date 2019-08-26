df93bc5fccd0e123dd7a58996ffc33aed9d67a02
ivy liu <ivy.liu.liuwanying@outlook.com>
2019-08-26 23:49:32 +0800

    234

--- 0415_01_se.py
+++ 0415_01_se.py
@@ -9,27 +9,27 @@ driver=webdriver.Chrome()
 
 
 # # 打开浏览器，同时打开首页
-url = 'https://jc-lab.yscredit.com/'
+# url = 'https://jc-lab.yscredit.com/'
 # # driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
 # driver = webdriver.Chrome()#上面一行没问题
-driver.get(url)
+# driver.get(url)
 
-#浏览器最大化
-driver.maximize_window()
+# #浏览器最大化
+# driver.maximize_window()
 #设置宽高
 driver.set_window_size(1333,888)
 
-#获取窗口标题
-print('窗口标题是-',driver.title)
+# #获取窗口标题
+# print('窗口标题是-',driver.title)
 
 
-#截图
-path='screenshot_pic//'+'1.png'
-driver.get_screenshot_as_file(path)
+# #截图
+# path='screenshot_pic//'+'1.png'
+# driver.get_screenshot_as_file(path)
 
-#刷新
-driver.refresh()
-time.sleep(5)
+# #刷新
+# driver.refresh()
+# time.sleep(5)
 driver.get('https://www.lizhi.fm/user/2525632519289351212')
 print('窗口标题是-',driver.title)
 
@@ -82,4 +82,43 @@ time.sleep(5)
 
 
 #xpath
-driver.get('http://xqtesting.com/')
+# driver.get('https://www.lizhi.fm/')
+# 关于 xpath 的使用可以用如下几种方式进行定位： 
+# 1）首推使用使用火狐下的 firepath 插件，简单、快捷、高效。chrome 下是先识别到
+# 元素的 html 代码在右键 copy>copy xpath 即可。
+# 2）自己手写 xpath 路径。一般语法格式为
+# //标签名[@属性名=属性值] 或者 //*[@属性名=属性值]
+# 区别在于是否有指定的标签
+
+#绝对定位
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div[1]/a/img").click()
+
+time.sleep(5)
+
+# #利用元素属性定位
+# driver.find_element_by_xpath(".//*[@id='js-obsBtn']").click()
+# time.sleep(5)
+
+#层级和属性结合
+driver.find_element_by_xpath("//*[@id='promoRadio']/div[3]/ul/li[1]/a/img").click()
+time.sleep(5)
+
+#使逻辑运算符 这个木有
+# driver.find_element_by_xpath(".//*[@class='audioName'and@class='form-control']")
+
+driver.get('https://www.guokr.com/')
+
+# driver.get('http://xqtesting.com/')
+
+#匹配部分属性  stars-with  ends-with
+# #查找id属性中开始位置包含‘nice'关键字的页面元素
+# driver.find_element_by_xpath("//input[starts-with(@id,'nice')]")
+# driver.find_element_by_xpath("//input[ends-with(@id,'nice')]")
+
+#查找input标签 placeholder属性中包含'搜索'关键字的页面元素
+driver.find_element_by_xpath("//input[contains(@placeholder,'搜索')]").click()#选中搜索连输入框
+# driver.find_element_by_xpath("//span[contains(@class,'styled__ImageFilterMask-sc-1mpyx4-1 gKJlUN')]")
+time.sleep(5)
+
+#text()
+driver.find_element_by_xpath("//*[@id='app']") 
